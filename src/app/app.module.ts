539d58994ed50ed2f57019cf7382cabe443d183b
Mahmoud SabahAllah <mahmoud.sabahallah@gmail.com>
2022-08-16 20:12:20 +0400

    Load local module using webpack magic string

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { I18nModule } from './core/I18n';
 import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavBarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, NavBarComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [I18nModule.setLocale(), I18nModule.setLocaleId()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { enableProdMode } from '@angular/core';
-import { loadTranslations } from '@angular/localize';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import CookieUtils from './app/utils/CookieUtil';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -9,41 +7,6 @@ if (environment.production) {
   enableProdMode();
 }
 
-// First locale is default, add additional after it
-const availableLocales = ['en', 'ar'];
-
-let langCookie = CookieUtils.getCookie('lang');
-
-let locale =
-  availableLocales.find((l) => langCookie === l) ??
-  availableLocales.find((l) => navigator.language.startsWith(l)) ??
-  availableLocales[0];
-
-window.document.documentElement.lang = locale;
-if (locale === 'ar') {
-  window.document.documentElement.dir = 'rtl';
-} else {
-  window.document.documentElement.dir = 'ltr';
-}
-
-if (locale === availableLocales[0]) {
-  platformBrowserDynamic()
-    .bootstrapModule(AppModule)
-    .catch((err) => console.error(err));
-} else {
-  // fetch resources for runtime translations. this could also point to an API endpoint
-  fetch(`assets/i18n/messages.${locale}.json`)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error ${response.status}`);
-      }
-      return response.json();
-    })
-    .then((result) => {
-      loadTranslations(result);
-
-      platformBrowserDynamic()
-        .bootstrapModule(AppModule)
-        .catch((err) => console.error(err));
-    });
-}
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
